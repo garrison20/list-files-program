c1178ea13f5477b0a08ea78313d612259ebe8d7b
garrison20 <50597088+garrison20@users.noreply.github.com>
2019-12-28 10:45:24 -0500

    v1.1
    Changed path-name storage to be dynamically allocated.

--- my-ls.c
+++ my-ls.c
@@ -2,10 +2,14 @@
   This program is a simple copy of the terminal's "ls" function.
   Simply typing executing the program with no arguments will print
   all the contents of the folder. Executing the program with a name
-  passed in as a parameter will print "exists" in the terminal to 
-  tell the user the file/directory is in the current directory. If 
-  there are two arguments, then the first is the file to be checked
-  for existence and the second is the desired directory to check in.
+  passed in as a parameter will print "Found file" if the name exists. 
+  and belongs to a file, and will print the contents of a directory if 
+  the name belongs to a directory. If there are two arguments, then the 
+  first is the file to be checked for existence and the second is the 
+  desired directory to check in. This perform the same function as 
+  passing only one argument to the program, but this only works inside 
+  one of the current directory's immediate directories. The function 
+  can't search inside a directory two levels down, only one.
 */
 
 #include <stdio.h>
@@ -13,12 +17,16 @@
 #include <dirent.h>
 #include <string.h>
 
-void print_contents(char *, DIR *, struct dirent *);
-void search(char *, char *, DIR *, struct dirent *);
+void print_contents(char *);
+int search(char *, char *);
+char *make_pathname(char *, char *);
 
 /* This function parses through all the contents of a directory and prints the 
 name of every file/dir it finds */
-void print_contents(char *dir, DIR *curr_dir, struct dirent *file_parsing) {
+void print_contents(char *dir) {
+  DIR *curr_dir = NULL;
+  struct dirent *file_parsing = NULL;
+
   /* If curr_dir is NULL, there is either no dir/file in the current directory
     or we have finished parsing through all files/dirs in the current directory */
   if ((curr_dir = opendir(dir)) == NULL) {
@@ -28,19 +36,22 @@ void print_contents(char *dir, DIR *curr_dir, struct dirent *file_parsing) {
 
   while ((file_parsing = readdir(curr_dir)) != NULL) {
     printf("%s\n", file_parsing->d_name);
-  }  
+  }
 
   closedir(curr_dir);
 }
 
 /* This function parses through a directory and prints "File found" if the name being 
 searched is a file and prints the contents of a directory if the name is a directory */
-void search(char *dir, char *desired_name, DIR *curr_dir, struct dirent *file_parsing) {
-  char path[100];
+/*int search(char *dir, char *desired_name, DIR *curr_dir, struct dirent *file_parsing) {*/
+int search(char *dir, char *desired_name) {
+  char *path = NULL;
+  DIR *curr_dir = NULL, *temp_dir = NULL;
+  struct dirent *file_parsing = NULL, *temp_parsing = NULL;
   
   if ((curr_dir = opendir(dir)) == NULL) {
     perror("Error opening directory");
-    exit(1);
+    return 1;
   }
 
   while ((file_parsing = readdir(curr_dir)) != NULL) {
@@ -51,23 +62,41 @@ void search(char *dir, char *desired_name, DIR *curr_dir, struct dirent *file_pa
       } else if (file_parsing->d_type == DT_DIR) {
         /* Create string that will designate the path necessary for reaching the 
         desired directory for printing */
-        strcpy(path, dir);
-        strcat(path, "/");
-        strcat(path, file_parsing->d_name);
+        path = make_pathname(dir, file_parsing->d_name);
 
-        /* Make these variables null so we can use them in the next print call 
-        and save space */
-        curr_dir = NULL;
-        file_parsing = NULL;
+        if (path == NULL)
+          return 1;
 
-        print_contents(path, curr_dir, file_parsing);
-        exit(0);
-      } else 
+        print_contents(path);
+        free(path);
+        return 0;
+      } else {
         printf("Unknown file/device\n");
+        return 0;
+      }
     }
   }
 
-  closedir(curr_dir);
+  printf("File/Directory not found\n");
+
+  return 0;
+}
+
+char *make_pathname(char *left_side, char *right_side) {
+  char *output = NULL;
+
+  output = malloc(sizeof(*left_side) + 1);
+  if (output != NULL) {
+    strcpy(output, left_side);
+    strcat(output, "/");
+
+    output = realloc(output, sizeof(*output) + sizeof(*right_side) + 2);
+    if (output != NULL) {
+      strcat(output, right_side);
+    }
+  }
+
+  return output;
 }
 
 int main(int argc, char *argv[]) {
@@ -76,14 +105,14 @@ int main(int argc, char *argv[]) {
 
   /* If no arguments, then print all of the folder's contents*/
   if (argc == 1) {
-    print_contents(".", curr_dir, file_parsing);
+    print_contents(".");
   /* If there's one argument, then search for a file/dir which has that name */
   } else if (argc == 2) {
-    search(".", argv[1], curr_dir, file_parsing);
+    search(".", argv[1]);
   /* If there are three arguments, the first arg is the desired file/dir and 
   the second arg is the directory name */
   } else if (argc == 3) {
-    search(argv[2], argv[1], curr_dir, file_parsing);
+    search(argv[2], argv[1]);
   /* If there are more than 3 arguments, then simply alert the user */
   } else {
     printf("Too many arguments\n");
